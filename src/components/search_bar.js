9a443777a7a9df433b2578bc8d27ef8d171185df
Ygor Fonseca <ygor.luiz.fonseca@gmail.com>
2016-07-16 23:09:38 -0300

    Awesome Youtube Search Based on React

---
+++ src/components/search_bar.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTerm(term);
+  }
+
+  render() {
+    return (
+      <div className="search-bar">
+        <input type="text"
+        value={this.state.term}
+        onChange={event => this.onInputChange(event.target.value)}/>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, {Component} from 'react';
+import _ from 'lodash';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyBatqapGwlpNZXzXqhe-imVhEI-ns5yGt8';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Create a new component. This should produce some HTML
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('ideias radicais - democracia');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  };
+
+  render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term)}, 300);
+
+    return (
+      <div>
+        <SearchBar onSearchTerm={videoSearch}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+        onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+        videos={this.state.videos}/>
+      </div>
+    );
+  }
+}
+
+// Add HTML produced in the component on the DOM
+ReactDOM.render(<App/>, document.getElementById('container'));
